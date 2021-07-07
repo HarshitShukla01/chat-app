e5530b00d997812033974661e39e64dd3c7b9299
HarshitShukla01 <harshitshukla258@gmail.com>
2021-07-07 10:45:16 +0530

    initialize firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,9 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyC7aVxR68TdE8I61ZoVOIUN36pFZtFbLS4",
+    authDomain: "chat-web-app-4d673.firebaseapp.com",
+    databaseURL: "https://chat-web-app-4d673-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-4d673",
+    storageBucket: "chat-web-app-4d673.appspot.com",
+    messagingSenderId: "321398591158",
+    appId: "1:321398591158:web:c1f3ae04c84040b4cf6017"
+  };
