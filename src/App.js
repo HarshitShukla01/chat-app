07b957908d79a0c4d1dd3ddc3dae724172d41b3b
HarshitShukla01 <harshitshukla258@gmail.com>
2021-07-07 11:21:58 +0530

    created public and private route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss'
+import {Switch} from 'react-router';
+import Signin from './pages/Signin'
+import Home from './pages/Home'
+import PrivateRoute from './components/PrivateRoute'
+import PublicRoute from './components/PublicRoute'
 
 function App() {
   return (
-    <div>
-     hello
-    </div>
+    <Switch>
+      <PublicRoute path='/signin'><Signin/></PublicRoute>
+      <PrivateRoute path='/'><Home/></PrivateRoute>
+    </Switch>
   );
 }
 
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
-  var firebaseConfig = {
+import firebase from 'firebase/app';
+
+const config = {
     apiKey: "AIzaSyC7aVxR68TdE8I61ZoVOIUN36pFZtFbLS4",
     authDomain: "chat-web-app-4d673.firebaseapp.com",
     databaseURL: "https://chat-web-app-4d673-default-rtdb.firebaseio.com",
@@ -7,3 +9,6 @@
     messagingSenderId: "321398591158",
     appId: "1:321398591158:web:c1f3ae04c84040b4cf6017"
   };
+
+const app = firebase.intializeApp(config)
+
