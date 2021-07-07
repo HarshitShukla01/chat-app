ccb4524290190eaafbb5da4e0f821161eb5b80a7
HarshitShukla01 <harshitshukla258@gmail.com>
2021-07-07 23:27:43 +0530

    manage user with profile context

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import Signin from './pages/Signin'
 import Home from './pages/Home'
 import PrivateRoute from './components/PrivateRoute'
 import PublicRoute from './components/PublicRoute'
+import {ProfileProvider} from './context/profile.context'
+
 
 function App() {
   return (
+    <ProfileProvider>
     <Switch>
       <PublicRoute path='/signin'><Signin/></PublicRoute>
       <PrivateRoute path='/'><Home/></PrivateRoute>
     </Switch>
+    </ProfileProvider>
   );
 }
 
---
+++ src/context/profile.context.js
@@ -0,0 +1,53 @@
+import React,{createContext,useState,useContext,useEffect} from 'react'
+import {auth,database} from '../misc/firebase'
+
+const ProfileContext = createContext();
+
+export const ProfileProvider = ({children}) => {
+
+    const [profile,setProfile] = useState(null)
+    const [isLoading,setIsLoading] = useState(true)
+
+    useEffect(() => {
+    	let userRef;
+
+    	const authUnsub = auth.onAuthStateChanged((authobj) => {
+        
+        if(authobj){
+         
+         userRef = database.ref(`/profiles/${authobj.uid}`);
+         userRef.on('value',(snap) => {
+          const {name,createdAt} = snap.val();
+          const data = {
+          	    name: name,
+          	    createdAt: createdAt,
+            	uid : authobj.uid,
+                email : authobj.email
+            }
+            setProfile(data)
+            setIsLoading(false);         
+         })
+
+        }
+         else { 
+           
+           if(userRef){userRef.off()}
+           
+           setProfile(null);
+           setIsLoading(false); }
+
+    	})
+
+    	return ()=>{
+    		authUnsub();
+
+    		if(userRef){userRef.off()}
+    	}
+    }, [])
+
+	return <ProfileContext.Provider value={{isLoading,profile}}>{children}</ProfileContext.Provider>
+}
+
+
+
+export const useProfile = () => useContext(ProfileContext);
