c090f8c9dd265f98478bc330091c6b311d67447d
HarshitShukla01 <harshitshukla258@gmail.com>
2021-07-08 11:00:46 +0530

    useMediaQuery hook added for defining sidebar width

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import {Icon ,Button,Drawer} from 'rsuite';
-import {useModalState} from '../../misc/custom-hooks'
+import {useModalState,useMediaQuery} from '../../misc/custom-hooks'
 import Dashboard from '.'
 
 const DashboardToggle = () => {
 
 	const {isOpen,open,close} = useModalState()
-
+    const isMobile = useMediaQuery('(max-width: 992px)')
 	return (
 		<>
 		<Button block color="blue" onClick={open}><Icon icon="dashboard" /></Button>
-		<Drawer show={isOpen} onHide={close} placement="left"> <Dashboard/> </Drawer>
+		<Drawer full={isMobile} show={isOpen} onHide={close} placement="left"> <Dashboard/> </Drawer>
 		</>
 	)
 }
