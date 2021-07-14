a95384ad49deb56b7c71a15a6a0f57af957fb6f9
HarshitShukla01 <harshitshukla258@gmail.com>
2021-07-14 14:56:47 +0530

    created layout for home page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss'
 import {Switch} from 'react-router';
 import Signin from './pages/Signin'
-import Home from './pages/Home'
+import Home from './pages/Home/'
 import PrivateRoute from './components/PrivateRoute'
 import PublicRoute from './components/PublicRoute'
 import {ProfileProvider} from './context/profile.context'
---
+++ src/pages/Home/Chat.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
+import { Loader } from 'rsuite';
+import { useRooms } from '../../context/rooms.context';
+import { transformToArr } from '../../misc/helpers';
+import { auth } from '../../misc/firebase';
+
+const Chat = () => {
+
+  return (
+    <div>
+     Hello
+    </div>
+  );
+};
+
+export default Chat;
