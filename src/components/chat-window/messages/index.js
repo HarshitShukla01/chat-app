805457a441300d66dd9202f4b61ab00cb09cd5a3
HarshitShukla01 <harshitshukla258@gmail.com>
2021-07-15 15:10:19 +0530

    displayed messages inside chat

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -8,10 +8,31 @@ import MessageItem from './MessageItem';
 
 
 const Messages = () => {
+  const { chatId } = useParams();
+  const [messages, setMessages] = useState(null);
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessages = messages && messages.length > 0;
+
+  useEffect(() => {
+    const messagesRef = database.ref('/messages')
+
+    messagesRef.orderByChild('roomId').equalTo(chatId).on('value', snap => {
+          const data = transformToArrWithId(snap.val());
+          setMessages(data);
+    });
+
+   return () => {
+      messagesRef.off('value');
+    };
+
+  }, [chatId]);
+
+
   return (
-     <div>
-       Messages
-     </div>
+     <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>No messages yet</li>}
+      {canShowMessages && messages.map((msg) => <MessageItem key={msg.id} message={msg}/>)}
+    </ul>
   );
 };
 
--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,15 +1,14 @@
-import React,{useState,useCallback,useEffect} from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react';
+import { database } from './firebase';
 
-export function useModalState(defaultvalue = false) {
+export function useModalState(defaultValue = false) {
+  const [isOpen, setIsOpen] = useState(defaultValue);
 
-	const [isOpen, setIsOpen] = useState(defaultvalue)
+  const open = useCallback(() => setIsOpen(true), []);
+  const close = useCallback(() => setIsOpen(false), []);
 
-	const open = useCallback(() => setIsOpen(true),[])
-	const close = useCallback(() => setIsOpen(false),[])
-
-	return {isOpen,open,close}
+  return { isOpen, open, close };
 }
-
 
 export const useMediaQuery = query => {
   const [matches, setMatches] = useState(
@@ -28,3 +27,52 @@ export const useMediaQuery = query => {
 
   return matches;
 };
+
+export function usePresence(uid) {
+  const [presence, setPresence] = useState(null);
+
+  useEffect(() => {
+    const userStatusRef = database.ref(`/status/${uid}`);
+
+    userStatusRef.on('value', snap => {
+      if (snap.exists()) {
+        const data = snap.val();
+
+        setPresence(data);
+      }
+    });
+
+    return () => {
+      userStatusRef.off();
+    };
+  }, [uid]);
+
+  return presence;
+}
+
+export function useHover() {
+  const [value, setValue] = useState(false);
+
+  const ref = useRef(null);
+
+  const handleMouseOver = () => setValue(true);
+  const handleMouseOut = () => setValue(false);
+
+  useEffect(
+    () => {
+      const node = ref.current;
+      if (node) {
+        node.addEventListener('mouseover', handleMouseOver);
+        node.addEventListener('mouseout', handleMouseOut);
+      }
+      return () => {
+        node.removeEventListener('mouseover', handleMouseOver);
+        node.removeEventListener('mouseout', handleMouseOut);
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [ref.current] // Recall only if ref changes
+  );
+
+  return [ref, value];
+}
